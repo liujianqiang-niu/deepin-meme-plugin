b95ec6bd20a12c4afa06b8b6bfc843c2a600b8fe
Deepin Desktop Environment Team <uos@uniontech.com>
2026-08-14 23:08:03 +0800

    fix(build): update theme install list and translations for new 5 themes
    CMakeLists.txt install(DIRECTORY) updated from old 6 themes to new 5 themes (laser-hero, plant-guard, ninja-slash, cosmic-star, firework-burst). Translation .ts file regenerated with lupdate to capture new qsTr strings from the redesigned QML files (15 source texts, 6 new).
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- translations/deepin-meme-plugin_zh_CN.ts
+++ translations/deepin-meme-plugin_zh_CN.ts
@@ -12,22 +12,41 @@
 <context>
     <name>MemeMain</name>
     <message>
-        <location filename="../src/control-center-plugin/MemeMain.qml" line="14"/>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="12"/>
         <source>启用特效</source>
         <translation>启用特效</translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemeMain.qml" line="30"/>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="25"/>
         <source>特效主题</source>
         <translation>特效主题</translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemeMain.qml" line="50"/>
-        <source>预览</source>
-        <translation>预览</translation>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="47"/>
+        <source>效果预览</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemeMain.qml" line="68"/>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="74"/>
+        <source>音效音量：调节特效播放时的音量大小</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="81"/>
+        <source>静音</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="88"/>
+        <source>最大</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>预览</source>
+        <translation type="vanished">预览</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="67"/>
         <source>高级设置</source>
         <translation>高级设置</translation>
     </message>
@@ -35,32 +54,41 @@
 <context>
     <name>MemePreview</name>
     <message>
-        <location filename="../src/control-center-plugin/MemePreview.qml" line="47"/>
         <source>请先启用趣味壁纸并选择主题</source>
-        <translation>请先启用趣味壁纸并选择主题</translation>
+        <translation type="vanished">请先启用趣味壁纸并选择主题</translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemePreview.qml" line="59"/>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="51"/>
+        <source>选择主题后显示动态壁纸</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="51"/>
+        <source>请先启用趣味壁纸特效</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="142"/>
         <source>删除</source>
         <translation>删除</translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemePreview.qml" line="60"/>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="143"/>
         <source>新建</source>
         <translation>新建</translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemePreview.qml" line="61"/>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="144"/>
         <source>重命名</source>
         <translation>重命名</translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemePreview.qml" line="62"/>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="145"/>
         <source>移动</source>
         <translation>移动</translation>
     </message>
     <message>
-        <location filename="../src/control-center-plugin/MemePreview.qml" line="63"/>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="146"/>
         <source>复制</source>
         <translation>复制</translation>
     </message>

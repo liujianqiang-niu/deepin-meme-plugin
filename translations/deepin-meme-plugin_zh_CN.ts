921b4260f10fb17ca8720e4186bbbfcc73d26f17
Deepin Desktop Environment Team <uos@uniontech.com>
2026-08-14 22:18:50 +0800

    feat(i18n): add zh_CN translation build pipeline with qt_add_lrelease
    The translations/ directory was empty and no .qm files were generated, causing the control center log warning: 'meme can not find qm files for locales QList("zh_CN")'. Added qt_add_lrelease/qt_add_lupdate build rules and a zh_CN.ts source file with 11 translated strings extracted from the QML sources. Also removed the hardcoded include() path for DtkDConfigMacros since find_package(Dtk6DConfig) auto-imports dtk_add_config_meta_files.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ translations/deepin-meme-plugin_zh_CN.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Meme</name>
+    <message>
+        <location filename="../src/control-center-plugin/Meme.qml" line="11"/>
+        <source>趣味壁纸</source>
+        <translation>趣味壁纸</translation>
+    </message>
+</context>
+<context>
+    <name>MemeMain</name>
+    <message>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="14"/>
+        <source>启用特效</source>
+        <translation>启用特效</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="30"/>
+        <source>特效主题</source>
+        <translation>特效主题</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="50"/>
+        <source>预览</source>
+        <translation>预览</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemeMain.qml" line="68"/>
+        <source>高级设置</source>
+        <translation>高级设置</translation>
+    </message>
+</context>
+<context>
+    <name>MemePreview</name>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="47"/>
+        <source>请先启用趣味壁纸并选择主题</source>
+        <translation>请先启用趣味壁纸并选择主题</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="59"/>
+        <source>删除</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="60"/>
+        <source>新建</source>
+        <translation>新建</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="61"/>
+        <source>重命名</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="62"/>
+        <source>移动</source>
+        <translation>移动</translation>
+    </message>
+    <message>
+        <location filename="../src/control-center-plugin/MemePreview.qml" line="63"/>
+        <source>复制</source>
+        <translation>复制</translation>
+    </message>
+</context>
+</TS>
